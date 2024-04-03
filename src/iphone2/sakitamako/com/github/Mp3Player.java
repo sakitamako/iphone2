8bdb271f59d161fe086ce8e3fdc334ad7dd084fc
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-03 22:01:31 +0900

    演習1.2.3追加

--- src/iphone2/sakitamako/com/github/Mp3Player.java
+++ src/iphone2/sakitamako/com/github/Mp3Player.java
@@ -1,5 +1,9 @@
 package iphone2.sakitamako.com.github;
 
 public interface Mp3Player {
-
+	public abstract void play();
+	public abstract void stop();
+	public abstract void next();
+	public abstract void back();
 }
+
