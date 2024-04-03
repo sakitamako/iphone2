a99240fda6fe7d5d5077f8caf43a02091bd4b2fd
sakitamako <sa.919.1025.ki@gmail.com>
2024-04-03 22:06:21 +0900

    演習4追加

---
+++ src/iphone2/sakitamako/com/github/NewFunction.java
@@ -0,0 +1,9 @@
+package iphone2.sakitamako.com.github;
+
+public interface NewFunction {
+	public abstract void  call();
+	public abstract void mail();
+	public abstract void photo();
+	public abstract void internet();
+}
+
